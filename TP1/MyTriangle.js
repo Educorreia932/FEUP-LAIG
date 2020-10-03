c2ae5267d928c0c6abe2a3e29454ddc1229d1ac7
Educorreia932 <skelozard1@gmail.com>
2020-10-03 21:14:58 +0100

    Finished the triangle primitive

--- TP1/MyTriangle.js
+++ TP1/MyTriangle.js
@@ -3,7 +3,7 @@
  * @constructor
  * @param scene - Reference to MyScene object
  */
-class MyRectangle extends CGFobject {
+class MyTriangle extends CGFobject {
 	constructor(scene, x1, y1, x2, y2, x3, y3) {
         super(scene);
         this.x1 = x1;
@@ -21,15 +21,30 @@ class MyRectangle extends CGFobject {
         let b = Math.sqrt(Math.pow((this.x3 - this.x2), 2) + Math.pow((this.y3 - this.y2), 2) + Math.pow((this.z3 - this.z2), 2));
         let c = Math.sqrt(Math.pow((this.x3 - this.x1), 2) + Math.pow((this.y3 - this.y1), 2) + Math.pow((this.z3 - this.z1), 2));
 
+		let cosAlpha = (Math.pow(a, 2) - Math.pow(b, 2) + Math.pow(c, 2)) / (2 * a * c);
+		let sinAlpha = Math.sqrt(1 - Math.pow(cosAlpha, 2));
+
         this.vertices = [
             this.x1, this.y1, 0,	//0
 			this.x2, this.y2, 0,	//1
 			this.x3, this.y3, 0,	//2
-        ]
+		];
+
+		this.normals = [
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1
+		];
 
         this.indices = [
             0, 1, 2
-        ]
+		];
+		
+		this.texCoords = [
+			0, 0,
+			a, 0,
+			c * cosAlpha, c * sinAlpha
+		];
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
