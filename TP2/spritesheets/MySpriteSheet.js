5bb96a5cbb62dceaccf7d89abfb163c6a105e7ef
Educorreia932 <skelozard1@gmail.com>
2020-11-06 23:12:41 +0000

    Created basic spritesheet structures

---
+++ TP2/spritesheets/MySpriteSheet.js
@@ -0,0 +1,17 @@
+/**
+ * MySpriteSheet
+ * @constructor
+ */
+class MySpriteSheet {
+    constructor(texture, sizeM, sizeN) {
+
+    }
+
+    activacteCellMN(m, n) {
+
+    }
+
+    activacteCellP(p) {
+
+    }
+}
