a5fe93d648a23e744f4cc370fd27cb3fdbe0d2ab
Educorreia932 <skelozard1@gmail.com>
2020-11-17 10:49:55 +0000

    Patch and plane primitives classes done

--- TP2/primitives/MyPlane.js
+++ TP2/primitives/MyPlane.js
@@ -1,14 +1,27 @@
 class MyPlane extends CGFobject {
-    constructor(scene) {
+    constructor(scene, npartsU, npartsV) {
         super(scene);
+
+        this.narptsU = npartsU;
+        this.npartsV = npartsV;
+
         this.initBuffers();
     }
 
     initBuffers() {
+        let controlPoints = [
+            [[-0.5, 0.0, 0.5, 1],
+             [-0.5, 0.0, -0.5, 1]],
+            [[0.5, 0.0, 0.5, 1],
+             [0.5, 0.0, -0.5, 1]]
+        ];
 
+
+        let surface = new CGFnurbsSurface(1, 1, controlPoints);
+        this.object = new CGFnurbsObject(this.scene, this.npartsU, this.npartsV, surface);
     }
 
     display() {
-        
+        this.object.display();
     }
 }
